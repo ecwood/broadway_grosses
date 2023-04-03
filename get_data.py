aa4f246a33eb0dbe79fad7a53d5d993b1e94b4d2
E.C. Wood <wooderi@stanford.edu>
2023-04-02 21:41:07 -0700

    first commit

---
+++ get_data.py
@@ -0,0 +1,115 @@
+import os
+import json
+import argparse
+import random
+
+TEMP_DIR = 'temp/'
+
+SHOW_START_TAG = '<td data-label="Show" class="col-0">'
+SHOW_END_TAG = '</tr>'
+LINK_MARKER = '<a href='
+
+# Keys
+LINK_KEY = 'link'
+THEATER_KEY = 'theater'
+GROSS_KEY = 'gross'
+DIFF_KEY = 'diff'
+AVG_TICKET_KEY = 'avg_ticket'
+TOP_TICKET_KEY = 'top_ticket'
+SEATS_SOLD_KEY = 'seats_sold'
+SEATS_IN_THEATER_KEY = 'seats_in_theater'
+PERFORMANCES_KEY = 'num_performances'
+PREVIEWS_KEY = 'num_previews'
+CAPACITY_KEY = 'percent_capacity'
+DIFF_CAP_KEY = 'diff_capacity'
+
+def get_args():
+	arg_parser = argparse.ArgumentParser(description='get_data.py: get Playbill Broadway Grosses data')
+	arg_parser.add_argument('inputLink', type=str)
+	return arg_parser.parse_args()
+
+def get_webpage(link):
+	os.system('mkdir ' + TEMP_DIR)
+	rand_num = random.randint(0, 1000);
+	tempfile = TEMP_DIR + 'tempfile' + str(rand_num) + '.txt'
+	os.system('curl -s ' + link + ' --output ' + tempfile)
+
+	lines = []
+	for line in open(tempfile, 'r'):
+		lines.append(line.strip())
+
+	os.system('rm ' + tempfile)
+	os.system('rm -r ' + TEMP_DIR)
+	return lines
+
+def process_span_line(line):
+	return line.split('>')[1].split('<')[0]
+
+def process_grosses(lines):
+	index = 0
+	show_grosses = dict()
+	while index < len(lines):
+		show = dict()
+		showname = ""
+		while index < len(lines) and lines[index] != SHOW_START_TAG:
+			index += 1
+		index += 1
+
+		# Link
+		show[LINK_KEY] = lines[index].split('"')[1]
+		index += 1
+
+		# Showname
+		showname = process_span_line(lines[index])
+		index += 2
+
+		# Theater
+		show[THEATER_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Weekly Gross
+		show[GROSS_KEY] = process_span_line(lines[index])
+		index += 4
+
+		# Weekly Gross Difference
+		show[DIFF_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Average Ticket Cost
+		show[AVG_TICKET_KEY] = process_span_line(lines[index])
+		index += 1
+		show[TOP_TICKET_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Seats Sold
+		show[SEATS_SOLD_KEY] = process_span_line(lines[index])
+		index += 1
+		show[SEATS_IN_THEATER_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Performances and Previews
+		show[PERFORMANCES_KEY] = process_span_line(lines[index])
+		index += 1
+		show[PREVIEWS_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Capacity Percentage
+		show[CAPACITY_KEY] = process_span_line(lines[index])
+		index += 3
+
+		# Difference in Capacity Percentage
+		show[DIFF_CAP_KEY] = process_span_line(lines[index])
+		index += 1
+
+		show_grosses[showname] = show
+		print_dict(show_grosses)
+	return show_grosses
+
+def print_dict(jsondict):
+	print(json.dumps(jsondict, sort_keys = True, indent = 4))
+
+if __name__ == '__main__':
+	args = get_args()
+
+	file = get_webpage(args.inputLink)
+	print_dict(process_grosses(file))
